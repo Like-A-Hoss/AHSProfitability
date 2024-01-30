6647d2bdc13ce87445649cdea176a9540dc846ac
Like-A-Hoss <hapleswithdice@gmail.com>
2024-01-30 16:05:19 -0600

    Wrapping up for day

--- GoogleIntegration/collectTracker.py
+++ GoogleIntegration/collectTracker.py
@@ -4,7 +4,6 @@ class Tracker():
     def __init__(self):
         self.url = ""
         gc = gspread.service_account()
-        self.tracker = gc
 
     def get_url(self):
         return self.url
@@ -13,5 +12,7 @@ class Tracker():
         self.url = url_in
 
     def retreive_sheet(self):
-        self.tracker = gc.open_by_url(self.get_url())
+        self.tracker = gc.open_by_url(self.get_url()).sheet1
+        return self.tracker
+
 
